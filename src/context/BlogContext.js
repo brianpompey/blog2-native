74ae5cac5beb76c22038276e1b62a9d1f196a745
brianpompey <brianthescholar@gmail.com>
2022-02-03 18:02:47 -0500

    Adding edit functionality

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -47,8 +47,9 @@ const deleteBlogPost = dispatch => {
 };
 
 const editBlogPost = dispatch => {
-    return (id, title, content, callback) => {
-       // dispatch({ type: 'edit_blogpost', payload: { id, title, content }})
+    return async (id, title, content, callback) => {
+        await jsonServer.put(`/blogposts/${id}`, { title, content })
+        dispatch({ type: 'edit_blogpost', payload: { id, title, content }})
         if (callback) {
             callback();
         }
